feef8716a232af6e0b5ecc4cff7bbe3763c2f97c
joelsam2 <57608717+joelsam2@users.noreply.github.com>
2019-11-10 22:43:49 -0800

    Add files via upload

---
+++ nov-10/main.c
@@ -0,0 +1,328 @@
+#include <stdio.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "board_io.h"
+#include "common_macros.h"
+#include "gpio.h"
+#include "sj2_cli.h"
+
+#include "gpio_driver.h"
+#include "led_matrix.h"
+//#include <stdint.h>
+
+void rows(uint8_t row);
+void alien_init();
+
+uint8_t test_array1[16][64] = {
+
+    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+};
+
+uint8_t test_array2[16][64] = {
+
+    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+};
+
+// uint64_t test_array [16][64] = {};
+
+int main(void) {
+
+  // xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0, PRIORITY_LOW, NULL);
+  // xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1, PRIORITY_LOW, NULL);
+
+  // It is advised to either run the uart_task, or the SJ2 command-line (CLI), but not both
+  // Change '#if 0' to '#if 1' and vice versa to try it out
+#if 0
+  // printf() takes more stack space, size this tasks' stack higher
+  xTaskCreate(uart_task, "uart", (512U * 8) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
+#else
+  sj2_cli__init();
+  // UNUSED(uart_task); // uart_task is un-used in if we are doing cli init()
+#endif
+
+  // init_led_matrix();
+  alien_init();
+  // LPC_IOCON->P2_3 = 0;
+  // gpio_dir(2, 3, 0);
+
+  // LPC_IOCON->P0_29 = 0;
+  // gpio_dir(0, 29, 1);
+  while (1) {
+    for (int i = 0; i < 16; i++) {
+
+      LPC_GPIO2->SET = (1 << 6); // OE
+      rows(i);
+      LPC_GPIO2->PIN &= ~(1 << 8); // latch
+      // LPC_GPIO2->PIN |= (1 << 8);
+      for (int j = 0; j < 64; j++) {
+        
+        //code to turn on two pixels
+        // // lower panel
+        // if (test_array1[i][j])
+        //   LPC_GPIO0->PIN |= (1 << 10);
+        // else
+        //   LPC_GPIO0->PIN &= ~(1 << 10);
+
+        // if (test_array1[i][j])
+        //   LPC_GPIO0->PIN |= (1 << 1);
+        // else
+        //   LPC_GPIO0->PIN &= ~(1 << 1);
+
+        // if (test_array1[i][j])
+        //   LPC_GPIO0->PIN |= (1 << 18);
+        // else
+        //   LPC_GPIO0->PIN &= ~(1 << 18);
+
+        // // top panel
+        // if (test_array2[16 - i][j])
+        //   LPC_GPIO0->PIN |= (1 << 15);
+        // else
+        //   LPC_GPIO0->PIN &= ~(1 << 15);
+        // if (test_array2[16 - i][j])
+        //   LPC_GPIO2->PIN |= (1 << 9);
+        // else
+        //   LPC_GPIO2->PIN &= ~(1 << 9);
+        // if (test_array2[16 - i][j])
+        //   LPC_GPIO2->PIN |= (1 << 7);
+        // else
+        //   LPC_GPIO2->PIN &= ~(1 << 7);
+
+        // Code to turn on all columns
+        LPC_GPIO0->SET = (1 << 10);
+        LPC_GPIO0->SET = (1 << 1);
+        LPC_GPIO0->SET = (1 << 18);
+
+        LPC_GPIO0->CLR = (1 << 15);
+        LPC_GPIO2->SET = (1 << 9);
+        LPC_GPIO2->SET = (1 << 7);
+
+        LPC_GPIO0->PIN |= (1 << 16);  // clk
+        LPC_GPIO0->PIN &= ~(1 << 16); // clk
+      }
+
+      LPC_GPIO2->PIN |= (1 << 8); // latch
+      // LPC_GPIO2->PIN &= ~(1 << 8); // latch
+      LPC_GPIO2->CLR = (1 << 6); // OE
+      delay__ms(500);
+    }
+  }
+  puts("Starting RTOS");
+  return 0;
+}
+
+void rows(uint8_t row) {
+  volatile uint8_t d = (row >> 0) & 1;
+  volatile uint8_t c = (row >> 1) & 1;
+  volatile uint8_t b = (row >> 2) & 1;
+  volatile uint8_t a = (row >> 3) & 1;
+
+  printf("Row number: %d\n", row);
+  printf("d: %d\n", d);
+  printf("c: %d\n", c);
+  printf("b: %d\n", b);
+  printf("a: %d\n", a);
+
+  // a ? (LPC_GPIO0->PIN |= (1 << 6)) : (LPC_GPIO0->PIN &= ~(1 << 6));
+  // b ? (LPC_GPIO2->PIN |= (1 << 4)) : (LPC_GPIO2->PIN &= ~(1 << 4));
+  // c ? (LPC_GPIO1->PIN |= (1 < 30)) : (LPC_GPIO1->PIN &= ~(1 << 30));
+  // d ? (LPC_GPIO0->PIN |= (1 << 17)) : (LPC_GPIO0->PIN &= ~(1 << 17));
+
+  //delay__ms(20);
+  // d ? (printf("Lpc_d %d\n", LPC_GPIO0->PIN |= (1 << 6))) : (printf("Lpc_d %d\n", (LPC_GPIO0->PIN &= ~(1 << 6))));
+  // c ? (printf("Lpc_c %d\n", LPC_GPIO2->PIN |= (1 << 4))) : (printf("Lpc_c %d\n", LPC_GPIO2->PIN &= ~(1 << 4)));
+  // // b ? (printf("Lpc_b %d\n", LPC_GPIO1->PIN |= (1 < 30))) : (printf("Lpc_b %d\n", LPC_GPIO1->PIN &= ~(1 << 30)));
+  // b ? (printf("Lpc_b %d\n", LPC_GPIO1->PIN |= (1 << 30))) : (printf("Lpc_b %d\n", LPC_GPIO1->PIN &= ~(1 << 30)));
+  // a ? (printf("Lpc_a %d\n", LPC_GPIO0->PIN |= (1 << 17))) : (printf("Lpc_a %d\n", LPC_GPIO0->PIN &= ~(1 << 17)));
+
+  d ? (printf("Lpc_d %d\n", LPC_GPIO0->PIN |= (1 << 6))) : (printf("Lpc_d %d\n", (LPC_GPIO0->PIN &= ~(1 << 6))));
+  c ? (printf("Lpc_c %d\n", LPC_GPIO2->PIN |= (1 << 4))) : (printf("Lpc_c %d\n", LPC_GPIO2->PIN &= ~(1 << 4)));
+  b ? (printf("Lpc_b %d\n", LPC_GPIO1->PIN |= (1 << 30))) : (printf("Lpc_b %d\n", LPC_GPIO1->PIN &= ~(1 << 30)));
+  a ? (printf("Lpc_a %d\n", LPC_GPIO0->PIN |= (1 << 17))) : (printf("Lpc_a %d\n", LPC_GPIO0->PIN &= ~(1 << 17)));
+}
+
+void alien_init() {
+
+  // R1
+  LPC_IOCON->P0_10 = 0;
+  // G1
+  LPC_IOCON->P0_1 = 0;
+  // B1
+  LPC_IOCON->P0_18 = 0;
+  // R2
+  LPC_IOCON->P0_15 = 0;
+  // G2
+  LPC_IOCON->P2_9 = 0;
+  // B2
+  LPC_IOCON->P2_7 = 0;
+
+  // A
+  // LPC_IOCON->P0_11 = 0;
+  LPC_IOCON->P0_6 = 0;
+  // B
+  // LPC_IOCON->P0_0 = 0;
+  LPC_IOCON->P2_4 = 0;
+  // C
+  // LPC_IOCON->P0_22 = 0;
+  LPC_IOCON->P1_30 = 0;
+  // D
+  LPC_IOCON->P0_17 = 0;
+
+  // CLK
+  LPC_IOCON->P0_16 = 0;
+  // LAT
+  LPC_IOCON->P2_8 = 0;
+  // OE
+  LPC_IOCON->P2_6 = 0;
+
+  gpio_dir(0, 10, 0);
+  gpio_dir(0, 1, 0);
+  gpio_dir(0, 18, 0);
+  gpio_dir(0, 15, 0);
+  gpio_dir(2, 9, 0);
+  gpio_dir(2, 7, 0);
+  // gpio_dir(0, 11, 0);
+  gpio_dir(0, 6, 0);
+  // gpio_dir(0, 0, 0);
+  gpio_dir(2, 4, 0);
+  // gpio_dir(1, 30, 0);
+  LPC_GPIO1->DIR |= (1 << 30);
+  gpio_dir(0, 17, 0);
+  gpio_dir(0, 16, 0);
+  gpio_dir(2, 8, 0);
+  gpio_dir(2, 6, 0);
+}
+
+static void blink_task(void *params) {
+  const gpio_s led = *((gpio_s *)params);
+
+  // Warning: This task starts with very minimal stack, so do not use printf() API here to avoid stack overflow
+  while (true) {
+    gpio__toggle(led);
+    vTaskDelay(500);
+  }
+}
+
+// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
+static void uart_task(void *params) {
+  TickType_t previous_tick = 0;
+  TickType_t ticks = 0;
+
+  while (true) {
+    // This loop will repeat at precise task delay, even if the logic below takes variable amount of ticks
+    vTaskDelayUntil(&previous_tick, 2000);
+
+    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire output will be fully
+     * sent out before this function returns. See system_calls.c for actual implementation.
+     *
+     * Use this style print for:
+     *  - Interrupts because you cannot use printf() inside an ISR
+     *  - During debugging in case system crashes before all output of printf() is sent
+     */
+    ticks = xTaskGetTickCount();
+    fprintf(stderr, "%u: This is a polled version of printf used for debugging ... finished in", (unsigned)ticks);
+    fprintf(stderr, " %lu ticks\n", (xTaskGetTickCount() - ticks));
+
+    /* This deposits data to an outgoing queue and doesn't block the CPU
+     * Data will be sent later, but this function would return earlier
+     */
+    ticks = xTaskGetTickCount();
+    printf("This is a more efficient printf ... finished in");
+    printf(" %lu ticks\n\n", (xTaskGetTickCount() - ticks));
+  }
+}
