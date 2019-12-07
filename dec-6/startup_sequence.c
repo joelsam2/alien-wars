e64fd88fad19f6da886ec7f965074e83e40d0f02
joelsam2 <57608717+joelsam2@users.noreply.github.com>
2019-12-06 17:48:45 -0800

    Update startup_sequence.c

--- dec-6/startup_sequence.c
+++ dec-6/startup_sequence.c
@@ -18,121 +18,153 @@ extern uint8_t ready[8][64];
 extern uint8_t game_over_red[32][64];
 
 void burst_animation(uint8_t x, uint8_t y, uint8_t color) {
-  updatePixel(x, y, color);
-  updatePixel(x - 2, y + 1, color);
-  updatePixel(x + 2, y + 1, color);
-  updatePixel(x - 3, y + 3, color);
-  updatePixel(x + 1, y + 3, color);
-  updatePixel(x + 3, y + 4, color);
-  updatePixel(x - 1, y + 4, color);
-  updatePixel(x + 1, y + 5, color);
-  updatePixel(x, y + 7, color);
-  updatePixel(x - 2, y + 6, color);
+  // updatePixel(x, y, color);
+  // updatePixel(x - 2, y + 1, color);
+  // updatePixel(x + 2, y + 1, color);
+  // updatePixel(x - 3, y + 3, color);
+  // updatePixel(x + 1, y + 3, color);
+  // updatePixel(x + 3, y + 4, color);
+  // updatePixel(x - 1, y + 4, color);
+  // updatePixel(x + 1, y + 5, color);
+  // updatePixel(x, y + 7, color);
+  // updatePixel(x - 2, y + 6, color);
 
-  for (int i = 0; i < 40; i++) {
-    updateDisplay();
+  updatePixel(x, y, color);
+  updatePixel(x - 1, y, color);
+  updatePixel(x - 2, y, color);
+  updatePixel(x + 1, y, color);
+  updatePixel(x + 2, y, color);
+
+  updatePixel(x, y + 1, color);
+  updatePixel(x - 1, y + 1, color);
+  updatePixel(x - 2, y + 1, color);
+  updatePixel(x + 1, y + 1, color);
+  updatePixel(x + 2, y + 1, color);
+
+  updatePixel(x, y + 2, color);
+  updatePixel(x - 1, y + 2, color);
+  updatePixel(x - 2, y + 2, color);
+  updatePixel(x + 1, y + 2, color);
+  updatePixel(x + 2, y + 2, color);
+
+  updatePixel(x, y + 3, color);
+  updatePixel(x - 1, y + 3, color);
+  updatePixel(x - 2, y + 3, color);
+  updatePixel(x + 1, y + 3, color);
+  updatePixel(x + 2, y + 3, color);
+
+  updatePixel(x, y + 4, color);
+  updatePixel(x - 1, y + 4, color);
+  updatePixel(x - 2, y + 4, color);
+  updatePixel(x + 1, y + 4, color);
+  updatePixel(x + 2, y + 4, color);
+
+  updatePixel(x, y + 5, color);
+  updatePixel(x - 1, y + 5, color);
+  updatePixel(x - 2, y + 5, color);
+  updatePixel(x + 1, y + 5, color);
+  updatePixel(x + 2, y + 5, color);
+}
+void level_up_screen(uint8_t level_number) {
+
+  if (level_number == 2) {
+    for (int i = 0; i <= 14; i++) {
+      for (int j = 0; j < 64; j++) {
+        matrixbuff[i + 9][j] = level_two_red[i][j];
+      }
+    }
+
+    update_display_delay();
+
+    for (int i = 0; i <= 14; i++) {
+      for (int j = 0; j < 64; j++) {
+        matrixbuff[i + 9][j] = level_two_white[i][j];
+      }
+    }
+    update_display_delay();
+
+  } else if (level_number == 3) {
+    for (int i = 0; i <= 14; i++) {
+      for (int j = 0; j < 64; j++) {
+        matrixbuff[i + 9][j] = level_three_red[i][j];
+      }
+    }
+
+    update_display_delay();
+
+    for (int i = 0; i <= 14; i++) {
+      for (int j = 0; j < 64; j++) {
+        matrixbuff[i + 9][j] = level_three_white[i][j];
+      }
+    }
+    update_display_delay();
   }
 }
-  void level_up_screen(uint8_t level_number) {
 
-    if (level_number == 2) {
-      for (int i = 0; i <= 14; i++) {
-        for (int j = 0; j < 64; j++) {
-          matrixbuff[i + 9][j] = level_two_red[i][j];
-        }
-      }
+void game_over_screen(void) {
 
-      update_display_delay();
-
-      for (int i = 0; i <= 14; i++) {
-        for (int j = 0; j < 64; j++) {
-          matrixbuff[i + 9][j] = level_two_white[i][j];
-        }
-      }
-      update_display_delay();
-
-    } else if (level_number == 3) {
-      for (int i = 0; i <= 14; i++) {
-        for (int j = 0; j < 64; j++) {
-          matrixbuff[i + 9][j] = level_three_red[i][j];
-        }
-      }
-
-      update_display_delay();
-
-      for (int i = 0; i <= 14; i++) {
-        for (int j = 0; j < 64; j++) {
-          matrixbuff[i + 9][j] = level_three_white[i][j];
-        }
-      }
-      update_display_delay();
-    }
+  while (1) {
+    memcpy(matrixbuff, game_over_red, (1 * 32 * 64));
+    update_display_delay();
+    clearDisplay();
+    update_display_delay();
   }
+}
 
-  void game_over_screen(void) {
+void update_display_delay(void) {
 
-    while (1) {
-      memcpy(matrixbuff, game_over_red, (1 * 32 * 64));
-      update_display_delay();
-      clearDisplay();
-      update_display_delay();
-    }
-  }
+  if (!start_flag) {
+    for (int i = 0; i < 90; i++) {
+      updateDisplay();
 
-  void update_display_delay(void) {
-
-    if (!start_flag) {
-      for (int i = 0; i < 90; i++) {
-        updateDisplay();
-
-        if ((LPC_GPIO0->PIN & (1 << 29))) {
-          start_flag = true;
-          i = 100;
-          break;
-        }
-      }
-    }
-  }
-
-  void startup_screen(void) {
-    LPC_GPIO0->DIR &= ~(1 << 29);
-
-    while (1) {
-      memcpy(matrixbuff, startup_new, (1 * 32 * 64));
-      update_display_delay();
-      memcpy(matrixbuff, startup_new_two, (1 * 32 * 64));
-      update_display_delay();
-      if ((LPC_GPIO0->PIN & (1 << 29)) || (start_flag == true)) {
-        while (gpio_get(0, 29))
-          ;
+      if ((LPC_GPIO0->PIN & (1 << 29))) {
+        start_flag = true;
+        i = 100;
         break;
       }
     }
-
-    clearDisplay();
-    updateDisplay();
-    start_flag = false;
-
-    for (int i = 0; i <= 7; i++) {
-      for (int j = 0; j < 64; j++) {
-        matrixbuff[i + 11][j] = are[i][j];
-      }
-    }
-
-    update_display_delay();
-
-    for (int i = 0; i <= 7; i++) {
-      for (int j = 0; j < 64; j++) {
-        matrixbuff[i + 11][j] = you[i][j];
-      }
-    }
-    update_display_delay();
-
-    for (int i = 0; i <= 7; i++) {
-      for (int j = 0; j < 64; j++) {
-        matrixbuff[i + 11][j] = ready[i][j];
-      }
-    }
-    update_display_delay();
-    delay__ms(900);
   }
+}
+
+void startup_screen(void) {
+  LPC_GPIO0->DIR &= ~(1 << 29);
+
+  while (1) {
+    memcpy(matrixbuff, startup_new, (1 * 32 * 64));
+    update_display_delay();
+    memcpy(matrixbuff, startup_new_two, (1 * 32 * 64));
+    update_display_delay();
+    if ((LPC_GPIO0->PIN & (1 << 29)) || (start_flag == true)) {
+      while (gpio_get(0, 29))
+        ;
+      break;
+    }
+  }
+
+  clearDisplay();
+  updateDisplay();
+  start_flag = false;
+
+  for (int i = 0; i <= 7; i++) {
+    for (int j = 0; j < 64; j++) {
+      matrixbuff[i + 11][j] = are[i][j];
+    }
+  }
+
+  update_display_delay();
+
+  for (int i = 0; i <= 7; i++) {
+    for (int j = 0; j < 64; j++) {
+      matrixbuff[i + 11][j] = you[i][j];
+    }
+  }
+  update_display_delay();
+
+  for (int i = 0; i <= 7; i++) {
+    for (int j = 0; j < 64; j++) {
+      matrixbuff[i + 11][j] = ready[i][j];
+    }
+  }
+  update_display_delay();
+  delay__ms(900);
+}
